403894f9300e8cecac3b736f3d9f1bb01c94d565
MaximSaveliev <maxim.saveliev@mail.ru>
2024-02-08 23:41:05 +0200

    Add project files.

---
+++ DesignPatternSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns
+{
+    // Create DatabaseManager class and add sealed modifier to disable inheritance
+    public sealed class DatabaseManager
+    {
+        // Declare a static nullable _instance variable
+        private static DatabaseManager? _instance = null;
+        // Declare a static _lockCheck object to prevent cross threding instances
+        private static readonly object _lockCheck = new object();
+
+        // Private constructor (can initialize inside the class)
+        private DatabaseManager() { }
+
+        // Public static property witch represents the singleton instance
+        public static DatabaseManager Instance
+        {
+            // Add get accessor to prevent modification just reading
+            get
+            {
+                // Lock multiple thread instance initialization
+                lock(_lockCheck)
+                {
+                    // Create new DatabaseManager instance just when _instance is null
+                    if (_instance == null)
+                    {
+                        _instance = new DatabaseManager();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
---
+++ SingletonCheck.cs
@@ -0,0 +1,37 @@
+﻿using Design_Patterns;
+using System;
+
+DatabaseManager? instanceT1 = null;
+DatabaseManager? instanceT2 = null;
+
+// Initialize two threads and print their instances
+Thread t1 = new Thread(() =>
+{
+    instanceT1 = DatabaseManager.Instance;
+    Console.WriteLine($"Instance obtained by Thread 1: {instanceT1}");
+});
+
+Thread t2 = new Thread(() =>
+{
+    instanceT2 = DatabaseManager.Instance;
+    Console.WriteLine($"Instance obtained by Thread 2: {instanceT2}");
+});
+
+// Start threads and pause the execution of the main thread until t1 and t2 completes it's execution
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
+
+// Comparing instances obtained by each thread
+if (ReferenceEquals(instanceT1, instanceT2))
+{
+    Console.WriteLine("Both threads obtained the same instance. Singleton pattern confirmed.");
+}
+else
+{
+    Console.WriteLine("Instances obtained by threads are different.");
+}
+
+Console.WriteLine("Threads completed.");
